b32dac3b172cadbb3f7ffd739ed71baf6c5a07a7
dpaasch@my.wctc.edu <>
2013-02-23 12:48:39 -0600

    Update #3 - Added a Product[] array to the FakeDatabase class.

--- src/dmb/discountstrategy/FakeDatabase.java
+++ src/dmb/discountstrategy/FakeDatabase.java
@@ -1,24 +1,36 @@
 package dmb.discountstrategy;
 
 /**
- * This class will act as our database for the time being, holding both the customer
- * and the product arrays full of data.
+ * This class will act as our database for the time being, holding both the
+ * customer and the product arrays full of data.
  *
  * @author Dawn Bykowski
  */
 public class FakeDatabase {
 
+    // Customer Array
     private Customer[] customers = {
-        new Customer("1000", "Francesca Piccinini"),
-        new Customer("1001", "Dale Earnhardt Jr."),
-        new Customer("1002", "Natalie Gulbis"),
-        new Customer("1003", "Donald Driver"),
-        new Customer("1004", "Tina Charles"),
-        new Customer("1005", "Kevin Durant"),
-        new Customer("1006", "Michelle Wie"),
-        new Customer("1007", "Phil Mickelson"),
-        new Customer("1008", "Natalie Coughlin"),
-        new Customer("1009", "Ricky Fowler") };
+        new Customer("1001", "Francesca Piccinini"),
+        new Customer("1002", "Dale Earnhardt Jr."),
+        new Customer("1003", "Natalie Gulbis"),
+        new Customer("1004", "Donald Driver"),
+        new Customer("1005", "Tina Charles"),
+        new Customer("1006", "Kevin Durant"),
+        new Customer("1007", "Michelle Wie"),
+        new Customer("1008", "Phil Mickelson"),
+        new Customer("1009", "Natalie Coughlin"),
+        new Customer("1010", "Ricky Fowler")};
     
-    
+    // Products Array
+    private Product[] products = {
+        new Product("A100", "Queensize Comforter", 150.00, new FlatRateDiscount()),
+        new Product("A110", "Queensize Sheetset",  45.75, new NoDiscount()),
+        new Product("B101", "Men's Dress Shirt - Peacock Blue", 35.50, new QuantityDiscount()),
+        new Product("B200", "Men's Golf Slacks - Black", 44.75, new FlatRateDiscount()),
+        new Product("B278", "Men's Belt - Large", 18.95, new QuantityDiscount()),
+        new Product("C410", "Women's Dress - Medium", 38.50, new NoDiscount()),
+        new Product("C222", "Women's Tights - Navy", 12.50, new QuantityDiscount()),
+        new Product("D268", "Bath Towel - Beige", 18.95, new FlatRateDiscount()),
+        new Product("D345", "Hand Towel - Beige", 12.50, new NoDiscount()),
+        new Product("D654", "Washcloth - Beige", 12.50, new QuantityDiscount())};
 }
