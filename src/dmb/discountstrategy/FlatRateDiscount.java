e77b47e637e39a60fbaa02bf7d0ae620c11622fa
dpaasch@my.wctc.edu <>
2013-02-25 21:41:03 -0600

    Update #18 - Commented out the Main method in NoDiscount.  Added validation to the methods with parameters in the FlatRateDiscount class.

--- src/dmb/discountstrategy/FlatRateDiscount.java
+++ src/dmb/discountstrategy/FlatRateDiscount.java
@@ -2,18 +2,23 @@ package dmb.discountstrategy;
 
 /**
  * A Low-level class that calculates a discount
+ *
  * @author dawn bykowski
  */
 public class FlatRateDiscount implements DiscountStrategy {
+
     private double discountRate = .10;
 
+    // No-arg constructor
     public FlatRateDiscount() {
     }
 
+    // Constructor: accepts discountRate as parameter
     public FlatRateDiscount(double discountRate) {
-        this.discountRate = discountRate; 
+        this.discountRate = discountRate;
     }
 
+    // Accessor / Mutator Methods from Discount Strategy
     @Override
     public double getDiscountRate() {
         return discountRate;
@@ -21,14 +26,27 @@ public class FlatRateDiscount implements DiscountStrategy {
 
     @Override
     public void setDiscountRate(double discountRate) {
-        this.discountRate = discountRate;
+        // Validation of discountRate parameter
+        if (discountRate < 0.00 || discountRate > 0.00) {
+            System.out.println("Discount Rate cannot be greater than 0.00");
+        } else {
+            this.discountRate = discountRate;
+        }
     }
 
     @Override
     public double getDiscount(double cost, int quantity) {
+         if (cost <= 0.00) {
+            System.out.println("Cost must be greater than 0.00");
+        } 
+        if (quantity <= 0) {
+            System.out.println("Quantity must be greater than 0");
+        }
+        if (cost <= 0.00 && quantity <= 0) { 
+            System.out.println("Cost & Quantity must be greater than 0");
+        }
         return cost * quantity * discountRate;
     }
-    
 //    public static void main(String[] args) {
 //        FlatRateDiscount discount = new FlatRateDiscount();
 //        double amount = discount.getDiscount(10,2);
