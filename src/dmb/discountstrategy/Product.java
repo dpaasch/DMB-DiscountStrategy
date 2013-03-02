43f49ff96b01d45efd37db400362d26b1e9f9bae
dpaasch@my.wctc.edu <>
2013-03-02 12:48:00 -0600

    Update #26 - Made cosmetic changes to remove all unnecessary comments from Product class.

--- src/dmb/discountstrategy/Product.java
+++ src/dmb/discountstrategy/Product.java
@@ -15,8 +15,13 @@ public class Product {
 //    private QuantityDiscount discount;
 //    private NoDiscount discount;
     private DiscountStrategy discount;
+    
+    // No-arg constructor
+    public Product() {
+    }
 
-    // Constructor takes productId, productName, unitCost, and discount.
+    // Constructor: accepts productId, productName, unitCost, and discount as 
+    // parameters.
     //    public Product(String prodId, String prodName, double cost) {
     public Product(String productId, String productName, double unitCost,
             DiscountStrategy discount) {
@@ -29,12 +34,7 @@ public class Product {
         this.discount = discount;
     }
 
-    // No-arg constructor, in case it is needed.
-    public Product() {
-    }
-
-    /**
-     * This method gets the amount of the discount, after a quantity has been
+    /** This method gets the amount of the discount, after a quantity has been
      * provided.
      * @param quantity
      * @return the amount of the discount
@@ -46,16 +46,14 @@ public class Product {
         return discount.getDiscount(unitCost, quantity);
     }
 
-    /**
-     * This method gets the product id, which is a number unique to the product.
+    /** This method gets the product id, which is a number unique to the product.
      * @return the product identifier
      */
     public String getProductId() {
         return productId;
     }
 
-    /** 
-     * This method sets the product identifier to the value entered by the user.
+    /** This method sets the product identifier to the value entered by the user.
      * @param productId
      */
     public void setProductId(String productId) {
@@ -65,16 +63,14 @@ public class Product {
         this.productId = productId;
     }
 
-    /**
-     * This method gets the product name.
+    /** This method gets the product name.
      * @return the product name
      */
     public String getProductName() {
         return productName;
     }
 
-    /** 
-     * This method sets the product name to the value entered by the user.
+    /** This method sets the product name to the value entered by the user.
      * @param productName
      */
     public void setProductName(String productName) {
@@ -84,8 +80,7 @@ public class Product {
         this.productName = productName;
     }
 
-    /**
-     * This method gets the product unit cost to be used in calculating the
+    /** This method gets the product unit cost to be used in calculating the
      * discount later.
      * @return the unit cost
      */
@@ -93,8 +88,7 @@ public class Product {
         return unitCost;
     }
 
-    /**
-     * This method sets the product unit cost to the value entered by the user.
+    /** This method sets the product unit cost to the value entered by the user.
      * @param unitCost 
      */
     public void setUnitCost(double unitCost) {
