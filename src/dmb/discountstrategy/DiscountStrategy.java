588d52010ad0e6d17f59dce5f73358b4536040dc
dpaasch@my.wctc.edu <>
2013-03-02 11:35:52 -0600

    Update #23 - Added javadoc method comments to DiscountStrategy class.

--- src/dmb/discountstrategy/DiscountStrategy.java
+++ src/dmb/discountstrategy/DiscountStrategy.java
@@ -1,19 +1,29 @@
 package dmb.discountstrategy;
 
 /**
- * Lab #1 Activity: build just the strategy class hierarchy for the discount 
- * strategies. Now test it. Write a test class with a main method and use the 
- * liskov substitution principal and hard coded input to test that your strategy 
- * objects work as expected. 
- * 
- * @author Dawn Bykowski
+ * This is an abstraction class that is responsible for providing common discount 
+ * rate and discount calculation methods for the low-level classes to use.
+ *
+ * @author dawn bykowski, dpaasch@my.wctc.edu
  */
 public interface DiscountStrategy {
 
+    /** This method calculates the discount given the cost and the quantity. 
+     * @param cost
+     * @param quantity
+     * @return discount amount
+     */
     public abstract double getDiscount(double cost, int quantity);
 
+    /** This method gets the discount rate.
+     * @return the discounted rate
+     */
     public abstract double getDiscountRate();
 
+    /** This method sets the discounted rate to the value specified by the 
+     * parameter.
+     * @param discountRate
+     */
     public abstract void setDiscountRate(double discountRate);
     
 }
