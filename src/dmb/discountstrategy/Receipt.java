bd4a0c3502b786ab76c3620072a770277bbdd422
dpaasch@my.wctc.edu <>
2013-03-04 12:42:23 -0600

    Update #34 - Modified the getCustomer method validation within the Receipt class.

--- src/dmb/discountstrategy/Receipt.java
+++ src/dmb/discountstrategy/Receipt.java
@@ -15,33 +15,44 @@ public class Receipt {
     private LineItem[] lineItems = new LineItem[0];
     private Customer customer;
 
-    /** This method is responsible for obtaining the customer information from
+    /**
+     * This method is responsible for obtaining the customer information from
      * the database.
-     * @return customer number and customer name */
+     *
+     * @return customer number and customer name
+     */
     //Retrieve customer information from the database
     public String getCustomer(String customerId) {
-//        if (customerId != null) {
-//            System.out.println("Customer Number cannot be blank");
-//        }
-        FakeDatabase fakeDatabase = new FakeDatabase();
-        customer = fakeDatabase.findCustomer(customerId);
-        return customerId;
+        if (customerId != null) {
+            FakeDatabase fakeDatabase = new FakeDatabase();
+            customer = fakeDatabase.findCustomer(customerId);
+            return customerId;
+        } else {
+            System.out.println("Customer Number cannot be blank");
+        }
+        return null;
     }
 
-    /** This method generates a new line item and then adds it to the line item
+    /**
+     * This method generates a new line item and then adds it to the line item
      * array.
+     *
      * @param productId
-     * @param quantity */
+     * @param quantity
+     */
     public final void addLineItem(String productId, int quantity) {
         LineItem item = new LineItem(productId, quantity);
-          if(item != null){
-              addToArray(item);
-          }
+        if (item != null) {
+            addToArray(item);
+        }
     }
 
-    /** This method adds the previous line item to the array by creating a temp
+    /**
+     * This method adds the previous line item to the array by creating a temp
      * array to hold the additional line item to the previous fixed-sized array.
-     * @param item */
+     *
+     * @param item
+     */
     private final void addToArray(LineItem item) {
         LineItem[] tempItems = new LineItem[lineItems.length + 1];
         System.arraycopy(lineItems, 0, tempItems, 0, lineItems.length);
@@ -49,9 +60,12 @@ public class Receipt {
         lineItems = tempItems;
     }
 
-    /** This method gets the subtotal by totaling each of the line items before
+    /**
+     * This method gets the subtotal by totaling each of the line items before
      * the discount is applied.
-     * @return subtotal */
+     *
+     * @return subtotal
+     */
     public double getSubTotal() {
         double total = 0.00;  // Accumulator
         for (LineItem item : lineItems) {
@@ -60,9 +74,12 @@ public class Receipt {
         return total;
     }
 
-    /** This method gets the discount amount for the entire bill by totaling each
+    /**
+     * This method gets the discount amount for the entire bill by totaling each
      * of the line item discounts.
-     * @return amount saved (total discounts) */
+     *
+     * @return amount saved (total discounts)
+     */
     public double getDiscount() {
         double dTotal = 0.00;
         for (int i = 0; i < lineItems.length; i++) {
@@ -71,9 +88,12 @@ public class Receipt {
         return dTotal;
     }
 
-    /** This method gets the overall grand total of the bill by subtracting the
+    /**
+     * This method gets the overall grand total of the bill by subtracting the
      * total discounts from the subtotal.
-     * @return grand total */
+     *
+     * @return grand total
+     */
     public double getGrandTotal() {
         double grandTotal = 0.00;
         for (LineItem item : lineItems) {
@@ -81,15 +101,17 @@ public class Receipt {
         }
         return grandTotal;
     }
-    
-    /** This method generates the actual receipt.  It calls both the
-     * generateReceiptHeader and generateReceiptFooter private methods. */
+
+    /**
+     * This method generates the actual receipt. It calls both the
+     * generateReceiptHeader and generateReceiptFooter private methods.
+     */
     public final void generateReceipt() {
         generateReceiptHeader();
         System.out.println("  \t\t\tCustomer ID: " + customer.getCustomerId()
                 + "\tCustomer Name: " + customer.getCustomerName());
-        System.out.println("Id      " + "   Product Name               " 
-                + "   Unit Cost   " + "Quantity   " + "Extended Cost   " 
+        System.out.println("Id      " + "   Product Name               "
+                + "   Unit Cost   " + "Quantity   " + "Extended Cost   "
                 + "Amount Saved \n"
                 + "********   ***************************   *********   ********   "
                 + "*************   ************");
@@ -98,8 +120,10 @@ public class Receipt {
         }
         generateReceiptFooter();
     }
-    
-        /** This method generates the receipt header */
+
+    /**
+     * This method generates the receipt header
+     */
     private final void generateReceiptHeader() {
         System.out.println("  \t\t\t\t\t KOHL'S  ");
         System.out.println("  \t\t\t\tWestbrook Shopping Center");
@@ -111,9 +135,11 @@ public class Receipt {
         System.out.println("  \t\t\t  ID # 999-8983-8787-4675-9908-9828-9647  ");
         System.out.println();
     }
-    
-    /** This method generates the receipt footer which includes the subtotal,
-     * the total, and the amount saved. */
+
+    /**
+     * This method generates the receipt footer which includes the subtotal, the
+     * total, and the amount saved.
+     */
     private final void generateReceiptFooter() {
         System.out.println();
         System.out.printf(" \t\t\t\t\t\tSUBTOTAL  \t$%,.2f%n", getSubTotal());
@@ -123,9 +149,8 @@ public class Receipt {
         System.out.println(" \t\t\t\tTHANK YOU FOR SHOPPING AT KOHL'S");
         System.out.println("\n");
     }
-        
-    // Retrieve product information from the database - Removed so dependency 
-    // upon product is not required.
+      // Retrieve product information from the database - Removed so dependency 
+      // upon product is not required.
 //    public final void addItemToSale(String productId, int quantity) {
 //        FakeDatabase fakeDatabase = new FakeDatabase();
 //        Product product = fakeDatabase.findProduct(productId);
